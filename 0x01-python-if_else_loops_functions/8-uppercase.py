d4045758385679c1faabd42f1e48aa8209db1711
isybliss <isyblissumunna@gmail.com>
2022-11-29 04:23:32 -0800

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def uppercase(str):
+    result = ''
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            result = result + chr(ord(char) - 32)
+        else:
+            result = result + char
+        print("{:s}".format(result))
