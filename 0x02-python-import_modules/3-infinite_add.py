e0e6e6648af22e08447e10d4d641d5b1eb4f5ebd
isybliss <isyblissumunna@gmail.com>
2022-12-01 22:31:40 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-arg_num = len(sys.argv) - 1
-result = 0
-for i in range(arg_num):
-    result += int(sys.argv[i + 1])
-print("{}".format(result))
+
+import sys
+
+
+def main():
+    argv = sys.argv
+    arg_num = len(argv)
+    i = 1
+    res = 0
+
+    while (i < arg_num):
+        res += int(arg[i])
+        i += 1
+
+    print("{}".format(res))
+
+
+    if __name__ == "__main__":
+        main()
