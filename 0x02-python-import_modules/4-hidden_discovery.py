bc0ba0af93c4893046441eff4e806ca81831aa48
isybliss <isyblissumunna@gmail.com>
2022-12-01 22:15:55 +0100

    task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__":
+    """Print all names defined bt hidden_4 module."""
+    import hidden_4
 
-
-def discov():
     name = dir(hidden_4)
     for n in name:
-        if n[:2] != '__':
+        if n[:2] != "__":
             print(n)
-
-
-if __name__ == "__main__":
-    discov()
