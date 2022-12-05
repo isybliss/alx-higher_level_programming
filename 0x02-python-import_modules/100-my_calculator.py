6f3aedb92e404e697c6bd794252b2500164244d2
isybliss <isyblissumunna@gmail.com>
2022-12-05 10:27:00 +0100

    magic calculation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
     if arg_num != 3:
         print("Usuage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif sys.argv[2] not in '+-*/':
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
 
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if op is '+':
-        print("{} {} {} = {}".format(a, '+', b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, '-', b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, '*', b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, '/', b, div(a, b)))
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
