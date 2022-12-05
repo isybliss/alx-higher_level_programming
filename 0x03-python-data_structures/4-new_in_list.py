a6ae88e9f30d0091356a0318107cbc078d7e539f
isybliss <isyblissumunna@gmail.com>
2022-12-05 11:09:05 +0100

    data structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    """Make copy of a list, reolace element t a certain index in copy
+        while leaving original unmodified.
+    """
     new_list = my_list[:]
+
     if idx < o or idx > (len(my_list) - 1)):
-        return (my_list)
+        return (my_list[:])
 
     new_list[idk] = element
     return (new_list)
