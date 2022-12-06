c8097f06483db1af08a9106edf6620cb13295091
isybliss <isyblissumunna@gmail.com>
2022-12-06 09:55:29 +0100

    dat structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ""
-    for cha in my_string:
-        if (cha != 'c') and (ch != 'C'):
-            new_string += ch
-
-    return (new_string)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
