922d65fe028a81412cbac6d4dbf842e685ce0dd1
isybliss <isyblissumunna@gmail.com>
2022-12-07 22:39:58 +0100

    data structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    biggest = max(my_dict.values())
-    for key, value in my_dict.items():
+    biggest = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
         if value is biggest:
             return key
