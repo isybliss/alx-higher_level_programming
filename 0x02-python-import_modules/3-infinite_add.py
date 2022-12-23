64c136714407faddac905ec0f911703252ca2f5b
Isioma <101529722+isybliss@users.noreply.github.com>
2022-12-23 11:48:19 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,18 +3,10 @@
 import sys
 
 
-def main():
-    argv = sys.argv
-    arg_len = len(argv)
-    n = 1
-    sum_res = 0
-
-    while (n < arg_len):
-        sum_res += int(argv[n])
-        n += 1
-
-    print("{}".format(sum_res))
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
+    sum = 0
+    
+    for i in range(count):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
