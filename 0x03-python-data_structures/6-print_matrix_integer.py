b21dab1f7efc58552e86f15fb7a76393a48e8bd1
Isioma <101529722+isybliss@users.noreply.github.com>
2022-12-28 07:52:22 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,3 +9,6 @@ def print_matrix_integer(matrix=[[]]):
                 print(" ", end="")
 
         print()
+        
+        #for i in matrix:
+        #   print(" ".join("{:d}".format(j) for j in i))
