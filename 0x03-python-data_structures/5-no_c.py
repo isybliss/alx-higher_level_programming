90c0de9821306105a61964526e6d58d6255808b5
Isioma <101529722+isybliss@users.noreply.github.com>
2022-12-28 08:22:56 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,3 +2,11 @@
 def no_c(my_string):
     new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
+    
+    """
+    new_string = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
+    return new_string
+    """
