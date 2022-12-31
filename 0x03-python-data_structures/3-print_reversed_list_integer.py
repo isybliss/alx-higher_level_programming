8bea04b971d054eeaf07517400551c5c8a2f012f
Isioma <101529722+isybliss@users.noreply.github.com>
2022-12-31 13:51:41 +0100

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,3 +4,14 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         for n in my_list[::-1]:
             print("{:d}".format(n))
+            
+    """        
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
+    """
+    """
+    my_list = my_list[::-1]
+    for i in my_list:
+        print("{:d}".format(i))
+    """
