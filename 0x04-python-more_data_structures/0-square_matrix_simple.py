28cdc286883799addcacbe5d5e80a103e8ab79ef
Isioma <101529722+isybliss@users.noreply.github.com>
2023-01-02 00:50:01 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,3 +4,9 @@ def square_matrix_simple(matrix=[]):
     for x in matrix:
         new_matrix.append(list(map(lambda x: x**2, x)))
     return (new_matrix)
+    
+    """
+    for x in ([x**2 for x in row] for row in matrix):
+        new_matrix.append(x)
+    return new_matrix
+    """
