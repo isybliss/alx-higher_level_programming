ced4144d30ff80dbc313fb5c8e3ed82d6373f5b9
Isioma <101529722+isybliss@users.noreply.github.com>
2023-03-06 12:38:55 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,7 +59,8 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            for j in range(self.__width):
+                rect.append("#")
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
