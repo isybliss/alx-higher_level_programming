c5a352596c7f9b458df93a1f0fb36ccf489e8686
Isioma <101529722+isybliss@users.noreply.github.com>
2023-03-06 12:51:31 +0100

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -51,21 +51,16 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
-        Represents the rectangle with the # character.
-        """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
-
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        else:
+            rect = []
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    rect.append("#")
+                if i != self.__height - 1:
+                    rect.append("\n")
+            return ("".join(rect))
+        
     def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return (rect)
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
