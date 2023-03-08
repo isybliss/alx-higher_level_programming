1cbc42c34c935b75413b2660b193ada1ac0df9b2
Isioma <101529722+isybliss@users.noreply.github.com>
2023-03-08 09:39:39 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    return ("BestSchool, " * i + "BestSchool")
+    magic_string.n = getattr(magic_string, 'n', 0)
+    return ("BestSchool, " * magic_string.n + "BestSchool")
