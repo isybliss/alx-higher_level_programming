fcefcb947b7556d12829358bf698adadc7bbb534
isybliss <isyblissumunna@gmail.com>
2023-03-14 00:01:12 +0100

    increment

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+function incr () {
+  myObject.value += 1;
+  return (myObject.value);
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
