1cbf4b14cb9a73f82ff3246a8e650f208d62e0c7
isybliss <isyblissumunna@gmail.com>
2023-03-13 05:53:39 -0700

    C

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLen = myArray.length;
+
+for (let i = 0; i < arrayLen; i++) {
+  console.log(myArray[i]);
+}
