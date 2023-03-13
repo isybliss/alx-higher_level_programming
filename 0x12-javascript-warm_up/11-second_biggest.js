cf3284789ee54e8f427734bac019a601ed999954
isybliss <isyblissumunna@gmail.com>
2023-03-13 22:48:16 +0100

    add

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+function second (myArg) {
+  if (myArg.length === 2 || myArg.length === 3) {
+    return (0);
+  }
+  
+  let max = myArg[2];
+  let secondMax = myArg[3];
+
+  for (let i = 2; i < myArg.length; i++) {
+    if (myArg[i] > max) {
+	    secondMax = max;
+	    max = myArg[i];
+    } else if (myArg[i] > secondMax && myArg[i] < max) {
+	    secondMax = myArg[i];
+    }
+  }
+  return (secondMax);
+}
+
+
+console.log(second(process.argv));
