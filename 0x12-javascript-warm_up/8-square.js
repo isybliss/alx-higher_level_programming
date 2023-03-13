efb79dac308207599d8dccf9414e63763ff8fcf9
isybliss <isyblissumunna@gmail.com>
2023-03-13 06:14:09 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const num = process.argv[2];
+
+if (isNaN(parseInt(num))) {
+  console.log('Misssing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    let outPut = '';
+    for (let j = 0; j < num; j++) {
+      outPut += 'X';
+    }
+    console.log(outPut);
+  }
+}
