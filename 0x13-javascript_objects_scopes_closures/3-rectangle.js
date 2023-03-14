7f5aadd54617909f7629a4ad915faf14a7fc6b99
isybliss <isyblissumunna@gmail.com>
2023-03-14 11:38:36 +0100

    rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,17 +4,17 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    };
-    print() {
+    }
+  }
+
+  print () {
       let outPut = ''
       for (let i = 0; i < h; i++) {
         for (let j = 0; j < w; j++) {
           outPut = outPut + 'X';
-	};
+	}
 	console.log(outPut);
-      };
-    };
-  };
-}
+      }
+  }
 
 module.exports = Rectangle;
