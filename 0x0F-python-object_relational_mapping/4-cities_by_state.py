d42e44328ffa1e39a0baa1ca7a3fab39946193b9
isybliss <isyblissumunna@gmail.com>
2023-03-19 09:17:21 -0700

    update task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-lists all cities from the database hbtn_0e_4_usa
+script that lists all cities from the database hbtn_0e_4_usa
 """
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-
-    import MySQLdb
-    from sys import argv
-
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conn.cursor()
