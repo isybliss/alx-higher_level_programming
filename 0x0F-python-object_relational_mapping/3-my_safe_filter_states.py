99991edf2efd6f09cefdb8486edcc68615c5e7c8
isybliss <isyblissumunna@gmail.com>
2023-03-19 09:10:20 -0700

    update task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""
+script that takes in argument and displays all value
+in the state table of hbtn_0e_0_usa where name matches
+argument
+"""
 
 
 import MySQLdb
 from sys import argv
 
-'''
-script that lists all states from the database
-'''
 if __name__ == "__main__":
     cont = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
