1aee558fe69012cb5351affe955937938a5edb38
isybliss <isyblissumunna@gmail.com>
2023-04-04 16:05:14 -0700

    corrected my status:request package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,5 @@ if __name__ == '__main__':
     f = requests.get('https://alx-intranet.hbtn.io/status')
     status = f.text
     print("Body response:")
-    print("\t- table : {}\n\t- content: {}".format(type(status), status))
+    print("\t- table: {}".format(type(status)))
+    print("\t- content: {}".forma(status))
