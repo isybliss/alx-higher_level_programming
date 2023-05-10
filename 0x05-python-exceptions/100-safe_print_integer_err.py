79f6c575b1edf0506ee1feb95bbab69caf92bfcd
Isioma <101529722+isybliss@users.noreply.github.com>
2023-05-10 14:17:22 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,6 +8,7 @@ def safe_print_integer_err(value):
         return True
     except ValueError as err:
         print("Exception: {}".format(err), file=sys.stderr)
+        #sys.stderr.write("Exception: {}\n".format(err))
         return False
     except TypeError as err:
         print("Exception: {}".format(err), file=sys.stderr)
