55e746fa632a83d06feff38c30d3f3d7206657fc
Isioma <101529722+isybliss@users.noreply.github.com>
2023-05-10 12:25:36 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,3 +10,16 @@ def safe_print_list(my_list=[], x=0):
             return index
     print()
     return index
+
+"""
+    try:
+        index = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            index += 1
+     except IndexError:
+        pass
+     finally:
+        print()
+        return index
+"""
